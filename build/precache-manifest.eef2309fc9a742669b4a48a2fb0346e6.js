204b123b25dffca929b69cb8c681320ec0ad0238
snailthrone <eemeli.martti@live.fi>
2020-06-11 18:04:36 +0300

    added client build

---
+++ build/precache-manifest.eef2309fc9a742669b4a48a2fb0346e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28428b7caccb6ffc7006e03ce8efc988",
+    "url": "/index.html"
+  },
+  {
+    "revision": "973409fb7eeb10c6869f",
+    "url": "/static/css/main.675faa33.chunk.css"
+  },
+  {
+    "revision": "48026c1829382507f7d4",
+    "url": "/static/js/2.ee570c7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ee570c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973409fb7eeb10c6869f",
+    "url": "/static/js/main.4a7c81b5.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
